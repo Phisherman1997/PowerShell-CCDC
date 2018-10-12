a16472f63720e08fffefe350683c7ae05b1e3d65
Phisherman1997 <44077424+Phisherman1997@users.noreply.github.com>
2018-10-12 14:10:50 -0700

    Update Begin Info.ps1

---
+++ Begin Info.ps1
@@ -0,0 +1,12 @@
+﻿systeminfo > originalsysinfo.txt
+netstat –ano > originalnetstat.txt
+ipconfig /all > originalipconfig.txt
+route print > originalroutes.txt
+nbtstat –s > originalnbtstat.txt
+net user > originalusers.txt
+net localgroup > originalgroups.txt
+net group > originaldomaingroups.txt
+net share > originalnetshares.txt
+net session > originalsessions.txt
+tasklist > originalprocesses.txt
+net start > originalservices.txt
